7e30a890c68e68b82a88f7fc112ac986a963b483
James Lamb <jaylamb20@gmail.com>
2020-11-27 00:58:15 -0600

    re-render

--- mu_rprog/install_deps.R
+++ mu_rprog/install_deps.R
@@ -1,19 +1,10 @@
 # This script installs packages needed to run the examples
 # in the provided code supplements
-quit(status = 0)
-
-# "devtools" is needed to handle weird nonsense
-install.packages(
-    "devtools"
-    , repos = "http://cran.rstudio.com"
-    , dependencies = c("Depends", "Imports")
-    , verbose = TRUE
-    , quiet = FALSE
-)
 
 install.packages(
     pkgs = c(
-        "gaussfacts"
+        "devtools"
+        , "gaussfacts"
         , "htmltab"
         , "ineq"
         , "jsonlite"
