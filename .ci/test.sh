185f5372701363683bbd028cebe3bb8db6edb162
James Lamb <jaylamb20@gmail.com>
2020-11-27 21:39:11 -0600

    paths

--- .ci/test.sh
+++ .ci/test.sh
@@ -5,6 +5,6 @@ CRAN_MIRROR="https://cloud.r-project.org/"
 
 if [[ $TASK == "lint-r" ]]; then
     Rscript --vanilla -e "install.packages(c('httr', 'lintr'), repos = '${CRAN_MIRROR}', dependencies = c('Depends', 'Imports', 'LinkingTo'))" || exit -1
-    Rscript ${BUILD_DIRECTORY}/.ci/lint-r-code.R ${BUILD_DIRECTORY} || exit -1
+    Rscript .ci/lint-r-code.R ${BUILD_DIRECTORY} || exit -1
     exit 0
 fi
