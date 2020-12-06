c54debfd33fd936b7636c8666f42134952108b60
James Lamb <jaylamb20@gmail.com>
2020-12-05 19:08:56 -0600

    remove filter bubbles

--- mu_rprog/slides/assets/js/footer_bubbles.js
+++
@@ -1,8 +0,0 @@
-// Function for making progress-so-far footer bubbles for slidify slides
-function FooterBubbles(filled, tot_pages) {
-    document.write(
-        "&#x25cf ".repeat(filled) +
-        "&#x25cb ".repeat(tot_pages - filled) +
-        "&nbsp;"
-    );
-}
