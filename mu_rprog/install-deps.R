6e3f6dd19b7aed5c19508ffbab9bd84cab832927
James Lamb <jaylamb20@gmail.com>
2024-01-01 23:09:47 -0600

    [mu_rprog] pin to older version of 'markdown' (#144)

--- mu_rprog/install-deps.R
+++ mu_rprog/install-deps.R
@@ -1,6 +1,13 @@
 # This script installs packages needed to run the examples
 # in the provided code supplements
 
+if (!requireNamespace("slidify")) {
+    remotes::install_github("ramnathv/slidify", force = FALSE)
+}
+if (!requireNamespace("slidifyLibraries")) {
+    remotes::install_github("ramnathv/slidifyLibraries", force = FALSE)
+}
+
 install.packages(
     pkgs = c(
         "htmltab"
@@ -20,4 +27,12 @@ install.packages(
     , dependencies = c("Depends", "Imports")
     , verbose = TRUE
     , quiet = FALSE
+)
+
+# {markdown} v1.2.0 removed the markdownExtensions() function used by {slidify}
+# ref: https://github.com/rstudio/markdown/commit/6ef44310980aca1df52913678916d534fa739902
+install.packages(
+    "http://cran.r-project.org/src/contrib/Archive/markdown/markdown_1.1.tar.gz"
+    , repos = NULL
+    , type = "source"
 )
